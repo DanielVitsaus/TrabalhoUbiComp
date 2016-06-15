9f1b058c014bc3cf77c392c3d8da2ab51414ab4d
Daniel Andre <danielvitsaus@gmail.com>
2016-06-15 07:26:42 -0300

    Impelmentação do localização.

--- TiraDuvidas/app/src/main/java/br/com/tiradividas/CustomApplication.java
+++ TiraDuvidas/app/src/main/java/br/com/tiradividas/CustomApplication.java
@@ -1,18 +1,28 @@
 package br.com.tiradividas;
 
 import android.app.Application;
+import android.content.Context;
 import android.renderscript.Script;
+import android.support.multidex.MultiDex;
+import android.support.multidex.MultiDexApplication;
 
 import com.firebase.client.Firebase;
 
 /**
  * Created by daniel on 13/06/16.
  */
-public class CustomApplication extends Application {
+public class CustomApplication extends MultiDexApplication {
 
     @Override
     public void onCreate() {
         super.onCreate();
+        MultiDex.install(this);
         Firebase.setAndroidContext(this);
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+
     }
 }
--- TiraDuvidas/app/src/main/java/br/com/tiradividas/activityes/Localizacao.java
+++ TiraDuvidas/app/src/main/java/br/com/tiradividas/activityes/Localizacao.java
@@ -1,19 +1,36 @@
 package br.com.tiradividas.activityes;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
-import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.LocationServices;
 
 import br.com.tiradividas.MainActivity;
 import br.com.tiradividas.R;
 
-public class Localizacao extends MainActivity {
+public class Localizacao extends MainActivity implements GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener {
+
+    private TextView tvCoordinate;
+    private GoogleApiClient mGoogleApiClient;
+    private final int MY_LOCATION_REQUEST_CODE = 1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,6 +38,8 @@ public class Localizacao extends MainActivity {
         setContentView(R.layout.activity_localizacao);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        tvCoordinate = (TextView) findViewById(R.id.localizacao);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         if (fab != null) {
@@ -37,13 +56,75 @@ public class Localizacao extends MainActivity {
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         if (drawer != null) {
-            drawer.setDrawerListener(toggle);
+            drawer.addDrawerListener(toggle);
         }
         toggle.syncState();
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         if (navigationView != null) {
             navigationView.setNavigationItemSelectedListener(this);
+        }
+
+        callConnection();
+    }
+
+    private synchronized void callConnection() {
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addOnConnectionFailedListener(this)
+                .addConnectionCallbacks(this)
+                .addApi(LocationServices.API)
+                .build();
+        mGoogleApiClient.connect();
+    }
+
+    @SuppressLint("SetTextI18n")
+    @Override
+    public void onConnected(@Nullable Bundle bundle) {
+
+        Log.i("LOG", "onConnected(" + bundle + ")");
+
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    ActivityCompat#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for ActivityCompat#requestPermissions for more details.
+            return ;
+        }
+        Location l = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+
+        if(l != null){
+            Log.i("LOG", "latitude: "+l.getLatitude());
+            Log.i("LOG", "longitude: "+l.getLongitude());
+            tvCoordinate.setText(l.getLatitude()+" | "+l.getLongitude());
+        }
+
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+        Log.i("LOG", "onConnectionSuspended(" + i + ")");
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+        Log.i("LOG", "onConnectionFailed("+connectionResult+")");
+        //pararConexaoComGoogleApi();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        //pararConexaoComGoogleApi();
+    }
+
+    public void pararConexaoComGoogleApi() {
+        //Verificando se está conectado para então cancelar a conexão!
+        if (mGoogleApiClient.isConnected()) {
+            mGoogleApiClient.disconnect();
         }
     }
 
