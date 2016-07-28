e0b76c79ad32c6175d958fe8479fbb1462dd7261
Daniel Andre <danielvitsaus@gmail.com>
2016-07-28 17:00:17 -0300

    Tela do perfil criada.

--- TiraDuvidas/app/src/main/java/br/com/tiradividas/activityes/Perfil.java
+++ TiraDuvidas/app/src/main/java/br/com/tiradividas/activityes/Perfil.java
@@ -1,21 +1,51 @@
 package br.com.tiradividas.activityes;
 
+import android.app.ProgressDialog;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.design.widget.NavigationView;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+
+import java.util.List;
+import java.util.Map;
+
 import br.com.tiradividas.MainActivity;
+import br.com.tiradividas.Model.User;
 import br.com.tiradividas.R;
 import br.com.tiradividas.util.LibraryClass;
 
 public class Perfil extends MainActivity {
+    Firebase firebase = LibraryClass.getFirebase();
+    private static User user = LibraryClass.getUser();
+    private ProgressDialog dialog = null;
+    private AutoCompleteTextView name;
+    private AutoCompleteTextView email;
+    private AutoCompleteTextView escolaridade;
+    private AutoCompleteTextView idade;
+    private AutoCompleteTextView password;
+    private String matDIF;
+    private String matDOM;
+    private Spinner mat_dif;
+    private Spinner mat_dom;
+    private String[] materias = new String[]{"Selecione uma Materia","Cálculo I", "Cálculo II", "Cálculo III","Física I","Física II" ,"Física III", "Algoritmo", "Química",
+            "Anatomia"};
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_perfil);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
@@ -36,6 +66,124 @@ public class Perfil extends MainActivity {
             ((TextView)view.findViewById(R.id.emailuser)).setText(LibraryClass.getUser().getEmail());
             navigationView.setNavigationItemSelectedListener(this);
         }
+        name = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextView);
+        name.setText(user.getNome());
+
+        email = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextView2);
+        email.setText(user.getEmail());
+
+        escolaridade = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextView3);
+        escolaridade.setText(user.getEscolaridade());
+
+        idade = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextView4);
+        idade.setText(user.getIdade());
+
+        password = (AutoCompleteTextView) findViewById(R.id.autoCompleteTextView5);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, materias);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_item);
+
+        mat_dif = (Spinner) findViewById(R.id.spinner);
+        mat_dom = (Spinner) findViewById(R.id.spinner2);
+
+        mat_dif.setAdapter(adapter);
+        mat_dif.setSelection(adapter.getPosition(user.getMateria_dificuldade()));
+        mat_dif.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                matDIF = materias[position];
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        mat_dom.setAdapter(adapter);
+
+        mat_dom.setSelection(adapter.getPosition(user.getMateria_domoinio()));
+        mat_dom.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                if (position != 0)
+                    matDOM = materias[position];
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        Button cadastra = (Button) findViewById(R.id.button);
+
+        if (cadastra != null) {
+            cadastra.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    sendPerfilData();
+                }
+            });
+        }
+    }
+
+    public void sendPerfilData(  ){
+        //openProgressBar();
+        this.dialog = new ProgressDialog(this);
+        dialog.setMessage("Salvando...");
+        dialog.setCanceledOnTouchOutside(false);
+        dialog.setCancelable(false);
+        dialog.show();
+        this.alterUser();
+    }
+
+    protected void alterUser(){
+        user.setNome( name.getText().toString() );
+        user.setEmail( email.getText().toString() );
+        user.setEscolaridade( escolaridade.getText().toString());
+        user.setIdade( idade.getText().toString());
+        user.setMateria_dificuldade(matDIF);
+        user.setMateria_domoinio(matDOM);
+        if(password.getText().length()>0) {
+            user.setSenha(password.getText().toString());
+        }
+        user.saveDB();
+        dialog.hide();
+
+    }
+
+    private void saveUser(){
+
+        this.firebase.createUser(user.getEmail(),user.getSenha(),
+                new Firebase.ValueResultHandler<Map<String, Object>>() {
+                    @Override
+                    public void onSuccess(Map<String, Object> stringObjectMap) {
+                        //LibraryClass.saveSP(SignUpActivity.this, IDUSER , stringObjectMap.get("uid").toString());
+                        //LibraryClass.saveSP(SignUpActivity.this, EMAILUSER , user.getEmail());
+                        user.setId( stringObjectMap.get("uid").toString() );
+                        user.saveDB();
+                        //new SalvarUser().execute(user);
+                        firebase.unauth();
+
+                        //showToast( "Conta criada com sucesso!" );
+                        //closeProgressBar();
+                        if (dialog.isShowing()) {
+                            dialog.dismiss();
+                        }
+                        finish();
+                    }
+
+                    @Override
+                    public void onError(FirebaseError firebaseError) {
+                        //showToast( firebaseError.getMessage() );
+                        //closeProgressBar();
+                        if (dialog.isShowing()) {
+                            dialog.dismiss();
+                        }
+                    }
+                }
+        );
     }
 
 }
